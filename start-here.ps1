0b30009d6d9916d805f40967097ea6d818f60f0a
kindtek <98300737+kindtek@users.noreply.github.com>
2023-02-24 09:44:02 -0800

    fix variable naming error

--- start-here.ps1
+++ start-here.ps1
@@ -1,5 +1,5 @@
-$ScriptFromGitHub1 = Invoke-WebRequest 'https://raw.githubusercontent.com/kindtek/docker-to-wsl/dev/scripts/docker-wsl-install.ps1' -OutFile "docker-wsl-install.ps1"
-Invoke-Expression $($ScriptFromGitHub1.Content)
+$ScriptFromGitHub = Invoke-WebRequest https://raw.githubusercontent.com/kindtek/docker-to-wsl/dev/scripts/docker-wsl-install.ps1 -OutFile "docker-wsl-install.ps1"
+Invoke-Expression $($ScriptFromGitHub.Content)
 $WebClient = New-Object System.Net.WebClient
 $WebClient.DownloadFile("https://raw.githubusercontent.com/kindtek/docker-to-wsl/dev/scripts/get-latest-winget.ps1", "get-latest-winget.ps1")
 # $ScriptFromGitHub2 = Invoke-WebRequest https://raw.githubusercontent.com/kindtek/docker-to-wsl/dev/scripts/get-latest-winget.ps1 -OutFile "docker-wsl-install.ps1"
