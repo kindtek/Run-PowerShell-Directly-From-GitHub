5ddf3ee62e3cf581a47ad964bce1eb2cce52c07a
kindtek <98300737+kindtek@users.noreply.github.com>
2023-02-24 11:46:33 -0800

    added instructions in comments

--- start-here.ps1
+++ start-here.ps1
@@ -1,3 +1,6 @@
+# to set up your dev environment:
+# 1) open windows explorer and navigate to the directory you would like the docker-to-wsl repo to be cloned to
+# 2) right click, select open in terminal and then copy/pasta the contents of this file
 Invoke-WebRequest https://raw.githubusercontent.com/kindtek/docker-to-wsl/dev/scripts/docker-wsl-install.ps1 -OutFile "docker-wsl-install.ps1"
 $WebClient = New-Object System.Net.WebClient
 $WebClient.DownloadFile("https://raw.githubusercontent.com/kindtek/docker-to-wsl/dev/scripts/get-latest-winget.ps1", "get-latest-winget.ps1")
