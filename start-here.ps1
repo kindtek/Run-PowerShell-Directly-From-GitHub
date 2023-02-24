530e987ca674f50d489a9db1f4a2f627f1d5d9d9
kindtek <98300737+kindtek@users.noreply.github.com>
2023-02-24 08:30:43 -0800

    add replace("\","\\").replace('"',"") to strings when calling start-process

--- start-here.ps1
+++ start-here.ps1
@@ -3,8 +3,6 @@ $pwd_path = Split-Path -Path $PSCommandPath
 # @TODO: update branch to main
 $docker_wsl_install_ps1 = "$pwd_path/docker-wsl-install.ps1"
 $get_latest_winget_ps1 = "$pwd_path/get-latest-winget.ps1"
-Invoke-Expression $((Invoke-WebRequest 'https://raw.githubusercontent.com/kindtek/docker-to-wsl/dev/scripts/docker-wsl-install.ps1' -OutFile $docker_wsl_install_ps1).Content)
+Invoke-Expression $((Invoke-WebRequest 'https://raw.githubusercontent.com/kindtek/docker-to-wsl/dev/scripts/docker-wsl-install.ps1' -OutFile $docker_wsl_install_ps1.replace("\", "\\").replace('"', "")).Content)
 New-Object System.Net.WebClient.DownloadFile('https://raw.githubusercontent.com/kindtek/docker-to-wsl/dev/scripts/get-latest-winget.ps1', $get_latest_winget_ps1 )
-# $ScriptFromGitHub2 = Invoke-WebRequest https://raw.githubusercontent.com/kindtek/docker-to-wsl/dev/scripts/get-latest-winget.ps1 -OutFile "docker-wsl-install.ps1"
-# Invoke-Expression $($ScriptFromGitHub2.Content)
-Start-Process -FilePath $docker_wsl_install_ps1
+Start-Process -FilePath $docker_wsl_install_ps1.replace("\", "\\").replace('"', "")
