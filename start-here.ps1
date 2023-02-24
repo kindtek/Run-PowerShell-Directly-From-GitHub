5889ed5b9c3c1a2bb5c2528fb354b7d3965e6621
kindtek <98300737+kindtek@users.noreply.github.com>
2023-02-24 08:06:56 -0800

    add full path

--- start-here.ps1
+++ start-here.ps1
@@ -1,8 +1,10 @@
 $PSCommandPath | Split-Path -Parent
-$ScriptFromGitHub1 = Invoke-WebRequest "https://raw.githubusercontent.com/kindtek/docker-to-wsl/dev/scripts/docker-wsl-install.ps1" -OutFile "docker-wsl-install.ps1"
-Invoke-Expression $($ScriptFromGitHub1.Content)
+$pwd_path = Split-Path -Path $PSCommandPath
+# @TODO: update branch to main
+$ScriptFromGitHub = Invoke-WebRequest "https://raw.githubusercontent.com/kindtek/docker-to-wsl/dev/scripts/docker-wsl-install.ps1" -OutFile "$pwd_path/docker-wsl-install.ps1"
+Invoke-Expression $($ScriptFromGitHub.Content)
 $WebClient = New-Object System.Net.WebClient
-$WebClient.DownloadFile("https://raw.githubusercontent.com/kindtek/docker-to-wsl/dev/scripts/get-latest-winget.ps1", "get-latest-winget.ps1")
+$WebClient.DownloadFile("https://raw.githubusercontent.com/kindtek/docker-to-wsl/dev/scripts/get-latest-winget.ps1", "$pwd_path/get-latest-winget.ps1")
 # $ScriptFromGitHub2 = Invoke-WebRequest https://raw.githubusercontent.com/kindtek/docker-to-wsl/dev/scripts/get-latest-winget.ps1 -OutFile "docker-wsl-install.ps1"
-# Invoke-Expression $($ScriptFromGitHub.Content2)
+# Invoke-Expression $($ScriptFromGitHub2.Content)
 ./docker-wsl-install.ps1
