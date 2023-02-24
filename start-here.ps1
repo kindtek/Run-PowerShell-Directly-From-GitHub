e7acfc6615c5c9e9dba17035a3deed344a887b40
kindtek <98300737+kindtek@users.noreply.github.com>
2023-02-24 06:56:06 -0800

    bugfix - use raw file

--- start-here.ps1
+++ start-here.ps1
@@ -1,2 +1,2 @@
-$ScriptFromGitHub = Invoke-WebRequest https://github.com/kindtek/docker-to-wsl/tree/main/scripts/docker-wsl-install.ps1
+$ScriptFromGitHub = Invoke-WebRequest https://raw.githubusercontent.com/kindtek/docker-to-wsl/main/scripts/docker-wsl-install.ps1
 Invoke-Expression $($ScriptFromGitHub.Content)
