0a18bdb8a96c0542b8656c625a6071766d6c9a96
kindtek <98300737+kindtek@users.noreply.github.com>
2023-02-24 10:00:07 -0800

    works perfectly

--- start-here.ps1
+++ start-here.ps1
@@ -1,16 +1,12 @@
-$ScriptFromGitHub = Invoke-WebRequest https://raw.githubusercontent.com/kindtek/docker-to-wsl/dev/scripts/docker-wsl-install.ps1 -OutFile "docker-wsl-install.ps1"
-Invoke-Expression $($ScriptFromGitHub.Content)
+Invoke-WebRequest https://raw.githubusercontent.com/kindtek/docker-to-wsl/dev/scripts/docker-wsl-install.ps1 -OutFile "docker-wsl-install.ps1"
 $WebClient = New-Object System.Net.WebClient
 $WebClient.DownloadFile("https://raw.githubusercontent.com/kindtek/docker-to-wsl/dev/scripts/get-latest-winget.ps1", "get-latest-winget.ps1")
-# $ScriptFromGitHub2 = Invoke-WebRequest https://raw.githubusercontent.com/kindtek/docker-to-wsl/dev/scripts/get-latest-winget.ps1 -OutFile "docker-wsl-install.ps1"
-# Invoke-Expression $($ScriptFromGitHub.Content2)
-    # clone docker-to-wsl repo
-    # $user_name = kindtek
-    # $repo_name = docker-to-wsl
-    git clone 'https://github.com/kindtek/docker-to-wsl.git' --branch dev
-    Set-Location docker-to-wsl
-    git submodule update --init
-    Set-Location ../
-    # Set-Location "$pwd_path/scripts/powershell-remote"
-    # Start-Process -FilePath start-here.ps1
+# $user_name = kindtek
+# $repo_name = docker-to-wsl
+git clone 'https://github.com/kindtek/docker-to-wsl.git' --branch dev
+# navigate to directory of repo
+Set-Location docker-to-wsl
+# return to original working dir
+git submodule update --init
+Set-Location ../
 ./docker-wsl-install.ps1
