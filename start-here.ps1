2daebe3f8462ff02b0bfce720a9e2b57374b21e9
kindtek <98300737+kindtek@users.noreply.github.com>
2023-02-24 07:36:36 -0800

    add download script

--- start-here.ps1
+++ start-here.ps1
@@ -1,2 +1,8 @@
-$ScriptFromGitHub = Invoke-WebRequest https://raw.githubusercontent.com/kindtek/docker-to-wsl/main/scripts/docker-wsl-install.ps1
-Invoke-Expression $($ScriptFromGitHub.Content)
+$PSCommandPath | Split-Path -Parent
+$ScriptFromGitHub1 = Invoke-WebRequest https://raw.githubusercontent.com/kindtek/docker-to-wsl/dev/scripts/docker-wsl-install.ps1 -OutFile "docker-wsl-install.ps1"
+Invoke-Expression $($ScriptFromGitHub1.Content)
+$WebClient = New-Object System.Net.WebClient
+$WebClient.DownloadFile("https://raw.githubusercontent.com/kindtek/docker-to-wsl/dev/scripts/get-latest-winget.ps1", "get-latest-winget.ps1")
+# $ScriptFromGitHub2 = Invoke-WebRequest https://raw.githubusercontent.com/kindtek/docker-to-wsl/dev/scripts/get-latest-winget.ps1 -OutFile "docker-wsl-install.ps1"
+# Invoke-Expression $($ScriptFromGitHub.Content2)
+./docker-wsl-install.ps1
