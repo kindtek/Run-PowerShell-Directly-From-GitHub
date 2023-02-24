ece3b990773009f0ff998c849c4a1cb63c5aed46
kindtek <98300737+kindtek@users.noreply.github.com>
2023-02-24 08:11:04 -0800

    more absolute path stuff

--- start-here.ps1
+++ start-here.ps1
@@ -7,4 +7,4 @@ $WebClient = New-Object System.Net.WebClient
 $WebClient.DownloadFile("https://raw.githubusercontent.com/kindtek/docker-to-wsl/dev/scripts/get-latest-winget.ps1", "$pwd_path/get-latest-winget.ps1")
 # $ScriptFromGitHub2 = Invoke-WebRequest https://raw.githubusercontent.com/kindtek/docker-to-wsl/dev/scripts/get-latest-winget.ps1 -OutFile "docker-wsl-install.ps1"
 # Invoke-Expression $($ScriptFromGitHub2.Content)
-./docker-wsl-install.ps1
+./$pwd_path/docker-wsl-install.ps1
