2f5bedd0620c62e0c4da19df4244f3fb8eba8e6b
kindtek <98300737+kindtek@users.noreply.github.com>
2023-02-24 09:15:21 -0800

    partially working

--- start-here.ps1
+++ start-here.ps1
@@ -1,10 +1,17 @@
 $PSCommandPath | Split-Path -Parent
 $pwd_path = Split-Path -Path $PSCommandPath
-# @TODO: update branch to main
-$ScriptFromGitHub = Invoke-WebRequest "https://raw.githubusercontent.com/kindtek/docker-to-wsl/dev/scripts/docker-wsl-install.ps1" -OutFile "$pwd_path/docker-wsl-install.ps1"
-Invoke-Expression $($ScriptFromGitHub.Content)
+$ScriptFromGitHub1 = Invoke-WebRequest https://raw.githubusercontent.com/kindtek/docker-to-wsl/dev/scripts/docker-wsl-install.ps1 -OutFile "docker-wsl-install.ps1"
+Invoke-Expression $($ScriptFromGitHub1.Content)
 $WebClient = New-Object System.Net.WebClient
-$WebClient.DownloadFile("https://raw.githubusercontent.com/kindtek/docker-to-wsl/dev/scripts/get-latest-winget.ps1", "$pwd_path/get-latest-winget.ps1")
+$WebClient.DownloadFile("https://raw.githubusercontent.com/kindtek/docker-to-wsl/dev/scripts/get-latest-winget.ps1", "get-latest-winget.ps1")
 # $ScriptFromGitHub2 = Invoke-WebRequest https://raw.githubusercontent.com/kindtek/docker-to-wsl/dev/scripts/get-latest-winget.ps1 -OutFile "docker-wsl-install.ps1"
 # Invoke-Expression $($ScriptFromGitHub.Content2)
+    # clone docker-to-wsl repo
+    $user_name = kindtek
+    $repo_name = docker-to-wsl
+    git clone 'https://github.com/kindtek/docker-to-wsl.git' --branch dev
+    git submodule update --init set-branch dev
+    Set-Location $pwd_path/scripts/powershell-remote
+    Start-Process -FilePath start-here.ps1
+    Write-Output "pwd_path:$pwd_path"
 ./docker-wsl-install.ps1
