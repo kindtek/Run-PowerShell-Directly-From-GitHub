8287cc8020cda69cfe940f115ab632e5d5381427
kindtek <98300737+kindtek@users.noreply.github.com>
2023-02-24 12:13:19 -0800

    move start-here.bat

---
+++ start-here.bat
@@ -0,0 +1,3 @@
+@REM not working at the moment. not sure if it would help anything since windows 10+ have powershell but..
+@REM @TODO: add capability to start dev environment with batch script
+Powershell.exe -executionpolicy remotesigned -File start-here.ps1
