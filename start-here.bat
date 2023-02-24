1c714e225a1e4bfcdb12496fea8407379d370b02
kindtek <98300737+kindtek@users.noreply.github.com>
2023-02-24 12:14:14 -0800

    fix typo

--- start-here.bat
+++ start-here.bat
@@ -1,3 +1,3 @@
-@REM not working at the moment. not sure if it would help anything since windows 10+ have powershell but..
+@REM not working at the moment. not sure if it would help anything since windows 10+ has powershell but..
 @REM @TODO: add capability to start dev environment with batch script
 Powershell.exe -executionpolicy remotesigned -File start-here.ps1
