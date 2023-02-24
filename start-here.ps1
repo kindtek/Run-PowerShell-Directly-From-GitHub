2e7b8dc81120682137527d2700a021488852f3f8
kindtek <98300737+kindtek@users.noreply.github.com>
2023-02-24 09:42:12 -0800

    try to fix cannot bind error

--- start-here.ps1
+++ start-here.ps1
@@ -1,5 +1,4 @@
-$PSCommandPath | Split-Path -Parent
-$ScriptFromGitHub1 = Invoke-WebRequest https://raw.githubusercontent.com/kindtek/docker-to-wsl/dev/scripts/docker-wsl-install.ps1 -OutFile "docker-wsl-install.ps1"
+$ScriptFromGitHub1 = Invoke-WebRequest 'https://raw.githubusercontent.com/kindtek/docker-to-wsl/dev/scripts/docker-wsl-install.ps1' -OutFile "docker-wsl-install.ps1"
 Invoke-Expression $($ScriptFromGitHub1.Content)
 $WebClient = New-Object System.Net.WebClient
 $WebClient.DownloadFile("https://raw.githubusercontent.com/kindtek/docker-to-wsl/dev/scripts/get-latest-winget.ps1", "get-latest-winget.ps1")
