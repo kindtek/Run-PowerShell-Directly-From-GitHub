614c91844e4c8411b35e602507141344b612283f
kindtek <98300737+kindtek@users.noreply.github.com>
2023-02-26 03:38:48 -0800

    rearrange

--- install.ps1
+++ install.ps1
@@ -8,20 +8,23 @@ $download2 = "get-latest-winget.ps1"
 $add_wsl_windows_features = "add-wsl-windows-features"
 $download3 = "$add_wsl_windows_features/$repo_src_name/add-features.ps1"
 
+$WebClient = New-Object System.Net.WebClient
+
+
 # make directory tree for incoming repo
 $null = New-Item -Path $repo_src_name -ItemType Directory -Force -ErrorAction SilentlyContinue 
-Push-Location $repo_src_name
-$null = New-Item -Path scripts -ItemType Directory -Force -ErrorAction SilentlyContinue 
-Push-Location scripts
-$null = New-Item -Path $add_wsl_windows_features -ItemType Directory -Force -ErrorAction SilentlyContinue 
-
-$WebClient = New-Object System.Net.WebClient
-$WebClient.DownloadFile("https://raw.githubusercontent.com/$dir_host/$download1", "$dir_local/$download1")
 Write-Host "`n`rDownloading: https://raw.githubusercontent.com/$dir_host/$download1`r`nDestination: $dir_local/$download1" -ForegroundColor Magenta 
-$WebClient.DownloadFile("https://raw.githubusercontent.com/$dir_host/$download2", "$dir_local/$download2")
+$WebClient.DownloadFile("https://raw.githubusercontent.com/$dir_host/$download1", "$dir_local/$download1")
+Push-Location $repo_src_name
+
+$null = New-Item -Path scripts -ItemType Directory -Force -ErrorAction SilentlyContinue 
 Write-Host "`n`rDownloading: https://raw.githubusercontent.com/$dir_host/$download2`r`nDestination: $dir_local/$download2" -ForegroundColor Magenta 
-$WebClient.DownloadFile("https://raw.githubusercontent.com/$repo_src_owner/$download3", "$add_wsl_windows_features/add-features.ps1")
+$WebClient.DownloadFile("https://raw.githubusercontent.com/$dir_host/$download2", "$dir_local/$download2")
+Push-Location scripts
+
+$null = New-Item -Path $add_wsl_windows_features -ItemType Directory -Force -ErrorAction SilentlyContinue 
 Write-Host "`n`rDownloading: https://raw.githubusercontent.com/$repo_src_owner/$download3`r`nDestination: $add_wsl_windows_features/add-features.ps1`n`r" -ForegroundColor Magenta 
+$WebClient.DownloadFile("https://raw.githubusercontent.com/$repo_src_owner/$download3", "$add_wsl_windows_features/add-features.ps1")
 
 Pop-Location
 Pop-Location
