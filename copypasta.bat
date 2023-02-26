a980a0108018d67af0ab09ea861d85c504ecb8ee
kindtek <98300737+kindtek@users.noreply.github.com>
2023-02-25 18:14:28 -0800

    better filename

--- copypasta.bat
+++ copypasta.bat
@@ -1,2 +1,2 @@
-powershell -executionpolicy remotesigned -Command "Invoke-WebRequest https://raw.githubusercontent.com/kindtek/powershell-remote/docker-to-wsl/copypasta.ps1 -OutFile copypasta.ps1; powershell -executionpolicy remotesigned -File copypasta.ps1"
+powershell -executionpolicy remotesigned -Command "Invoke-WebRequest https://raw.githubusercontent.com/kindtek/powershell-remote/docker-to-wsl/install-docker-wsl.ps1 -OutFile install-docker-wsl.ps1; powershell -executionpolicy remotesigned -File install-docker-wsl.ps1"
 
