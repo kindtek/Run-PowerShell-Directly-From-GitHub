26523c5f924a80df6eea85972c5030a21d924cad
kindtek <98300737+kindtek@users.noreply.github.com>
2023-02-26 03:35:34 -0800

    move pop-location

--- install.ps1
+++ install.ps1
@@ -15,9 +15,6 @@ $null = New-Item -Path scripts -ItemType Directory -Force -ErrorAction SilentlyC
 Push-Location scripts
 $null = New-Item -Path $add_wsl_windows_features -ItemType Directory -Force -ErrorAction SilentlyContinue 
 
-Pop-Location
-Pop-Location
-
 $WebClient = New-Object System.Net.WebClient
 $WebClient.DownloadFile("https://raw.githubusercontent.com/$dir_host/$download1", "$dir_local/$download1")
 Write-Host "`n`rDownloading: https://raw.githubusercontent.com/$dir_host/$download1`r`nDestination: $dir_local/$download1" -ForegroundColor Magenta 
@@ -25,6 +22,9 @@ $WebClient.DownloadFile("https://raw.githubusercontent.com/$dir_host/$download2"
 Write-Host "`n`rDownloading: https://raw.githubusercontent.com/$dir_host/$download2`r`nDestination: $dir_local/$download2" -ForegroundColor Magenta 
 $WebClient.DownloadFile("https://raw.githubusercontent.com/$repo_src_owner/$download3", "$add_wsl_windows_features/add-features.ps1")
 Write-Host "`n`rDownloading: https://raw.githubusercontent.com/$repo_src_owner/$download3`r`nDestination: $add_wsl_windows_features/add-features.ps1`n`r" -ForegroundColor Magenta 
+
+Pop-Location
+Pop-Location
 
 # return to original working dir
 $file = "scripts/$download1"
