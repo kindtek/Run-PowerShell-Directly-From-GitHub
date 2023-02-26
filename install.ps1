8b82e17adefbd09093e536cc34d8c06ed8353d5d
kindtek <98300737+kindtek@users.noreply.github.com>
2023-02-25 19:19:44 -0800

    make clone script more dynamic 2

--- install.ps1
+++ install.ps1
@@ -2,14 +2,16 @@ $repo_src_owner = 'kindtek'
 $repo_src_name = 'docker-to-wsl'
 $repo_src_branch = 'dev'
 $repo = "$repo_src_owner/$repo_src_name/$repo_src_branch"
+$download1 = "docker-wsl-install.ps1"
+$download2 = "get-latest-winget.ps1"
 $WebClient = New-Object System.Net.WebClient
-$WebClient.DownloadFile("https://raw.githubusercontent.com/$repo/scripts/docker-wsl-install.ps1", "docker-wsl-install.ps1")
-$WebClient.DownloadFile("https://raw.githubusercontent.com/$repo/scripts/get-latest-winget.ps1", "get-latest-winget.ps1")
+$WebClient.DownloadFile("https://raw.githubusercontent.com/$repo/scripts/$download1", "docker-wsl-install.ps1")
+$WebClient.DownloadFile("https://raw.githubusercontent.com/$repo/scripts/$download2", "get-latest-winget.ps1")
 git clone "https://github.com/$repo_src_owner/$repo_src_name.git" --branch $repo_src_br
 # navigate to directory of repo
 $null = New-Item -Path $repo_src_name -ItemType Directory -Force -ErrorAction SilentlyContinue 
 Set-Location $repo_src_name
 # return to original working dir
 git submodule update --init
-./../$repo_src_name.ps1
+./../$download1.ps1
 
