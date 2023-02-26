136a58f967eacb14b8cb8422bbea4248366d299e
kindtek <98300737+kindtek@users.noreply.github.com>
2023-02-26 03:45:09 -0800

    reorg download

--- install.ps1
+++ install.ps1
@@ -13,18 +13,19 @@ $WebClient = New-Object System.Net.WebClient
 
 # make directory tree for incoming repo
 $null = New-Item -Path $repo_src_name -ItemType Directory -Force -ErrorAction SilentlyContinue 
-Write-Host "`n`rDownloading: https://raw.githubusercontent.com/$dir_host/$download1`r`nDestination: $dir_local/$download1" -ForegroundColor Magenta 
-$WebClient.DownloadFile("https://raw.githubusercontent.com/$dir_host/$download1", "$dir_local/$download1")
 Push-Location $repo_src_name
-
 $null = New-Item -Path scripts -ItemType Directory -Force -ErrorAction SilentlyContinue 
-Write-Host "`n`rDownloading: https://raw.githubusercontent.com/$dir_host/$download2`r`nDestination: $dir_local/$download2" -ForegroundColor Magenta 
-$WebClient.DownloadFile("https://raw.githubusercontent.com/$dir_host/$download2", "$dir_local/$download2")
 Push-Location scripts
 
+Write-Host "`n`rDownloading: https://raw.githubusercontent.com/$dir_host/$download1`r`nDestination: $download1" -ForegroundColor Magenta 
+$WebClient.DownloadFile("https://raw.githubusercontent.com/$dir_host/$download1", "$dir_local/$download1")
+Write-Host "`n`rDownloading: https://raw.githubusercontent.com/$dir_host/$download2`r`nDestination: $download2" -ForegroundColor Magenta 
+$WebClient.DownloadFile("https://raw.githubusercontent.com/$dir_host/$download2", "$dir_local/$download2")
+
 $null = New-Item -Path $add_wsl_windows_features -ItemType Directory -Force -ErrorAction SilentlyContinue 
+Push-Location $add_wsl_windows_features
 Write-Host "`n`rDownloading: https://raw.githubusercontent.com/$repo_src_owner/$download3`r`nDestination: $add_wsl_windows_features/add-features.ps1`n`r" -ForegroundColor Magenta 
-$WebClient.DownloadFile("https://raw.githubusercontent.com/$repo_src_owner/$download3", "$add_wsl_windows_features/add-features.ps1")
+$WebClient.DownloadFile("https://raw.githubusercontent.com/$repo_src_owner/$download3", "$dir_local/$add_wsl_windows_features/add-features.ps1")
 
 Pop-Location
 Pop-Location
