c574f57660afd84a6edf5665977aed27a2215662
kindtek <98300737+kindtek@users.noreply.github.com>
2023-02-26 01:07:16 -0800

    remove git clone

--- install.ps1
+++ install.ps1
@@ -6,23 +6,18 @@ $dir_local = "$repo_src_name/scripts"
 $download1 = "docker-wsl-install.ps1"
 $download2 = "get-latest-winget.ps1"
 
-#TODO: move next line elsewhere
-git clone "https://github.com/$repo_src_owner/$repo_src_name.git" --branch $repo_src_branch
-# navigate to directory of repo
 # make directory tree for incoming repo
-# $null = New-Item -Path $dir_local -ItemType Directory -Force -ErrorAction SilentlyContinue 
-# $null = New-Item -Path $dir_local -ItemType Directory -Force -ErrorAction SilentlyContinue 
+$null = New-Item -Path $dir_local -ItemType Directory -Force -ErrorAction SilentlyContinue 
+$null = New-Item -Path $dir_local -ItemType Directory -Force -ErrorAction SilentlyContinue 
 
-# $WebClient = New-Object System.Net.WebClient
-# $WebClient.DownloadFile("https://raw.githubusercontent.com/$dir_host/$download1", "$dir_local/$download1")
-# Write-Host "`n`rDownloading: https://raw.githubusercontent.com/$dir_host/$download1`r`nDestination: $dir_local/$download1`n`r`n`r" -ForegroundColor Magenta 
-# $WebClient.DownloadFile("https://raw.githubusercontent.com/$dir_host/$download2", "$dir_local/$download2")
-# Write-Host "`n`rDownloading: https://raw.githubusercontent.com/$dir_host/$download2`r`nDestination: $dir_local/$download2`n`r`n`r" -ForegroundColor Magenta 
+$WebClient = New-Object System.Net.WebClient
+$WebClient.DownloadFile("https://raw.githubusercontent.com/$dir_host/$download1", "$dir_local/$download1")
+Write-Host "`n`rDownloading: https://raw.githubusercontent.com/$dir_host/$download1`r`nDestination: $dir_local/$download1`n`r`n`r" -ForegroundColor Magenta 
+$WebClient.DownloadFile("https://raw.githubusercontent.com/$dir_host/$download2", "$dir_local/$download2")
+Write-Host "`n`rDownloading: https://raw.githubusercontent.com/$dir_host/$download2`r`nDestination: $dir_local/$download2`n`r`n`r" -ForegroundColor Magenta 
 
 Set-Location $repo_src_name
 # return to original working dir
-#TODO: move next line elsewhere
-git submodule update --force --recursive --init --remote
 $file = "scripts/$download1"
 Write-Output $file
 powershell -Command $file
