79e881a76988fe855937a2e41246485c4c4d7343
kindtek <98300737+kindtek@users.noreply.github.com>
2023-02-25 21:07:29 -0800

    better org

--- install.ps1
+++ install.ps1
@@ -1,18 +1,25 @@
 $repo_src_owner = 'kindtek'
 $repo_src_name = 'docker-to-wsl'
 $repo_src_branch = 'dev'
-$repo = "$repo_src_owner/$repo_src_name/$repo_src_branch"
-$dir = "$repo/scripts/"
-$download1 = "$dir/docker-wsl-install.ps1"
-$download2 = "$dir/get-latest-winget.ps1"
+$dir_host = "$repo_src_owner/$repo_src_name/$repo_src_branch/scripts"
+$dir_local = "$repo_src_name/scripts"
+$download1 = "docker-wsl-install.ps1"
+$download2 = "get-latest-winget.ps1"
+
+# make directory tree for incoming repo
+$null = New-Item -Path $dir_local -ItemType Directory -Force -ErrorAction SilentlyContinue 
+$null = New-Item -Path $dir_local -ItemType Directory -Force -ErrorAction SilentlyContinue 
+
 $WebClient = New-Object System.Net.WebClient
-$WebClient.DownloadFile("https://raw.githubusercontent.com/$repo_src_owner/$dir/$download1", $download1)
-$WebClient.DownloadFile("https://raw.githubusercontent.com/$repo_src_owner/$dir/$download2", $download2)
-git clone "https://github.com/$repo_src_owner/$repo_src_name.git" --branch $repo_src_branch
+$WebClient.DownloadFile("https://raw.githubusercontent.com/$dir_host/$download1", "$dir_local/$download1")
+Write-Host "`n`rDownloading: https://raw.githubusercontent.com/$dir_host/$download1`r`nDestination: $dir_local/$download1`n`r`n`r" -ForegroundColor Magenta 
+$WebClient.DownloadFile("https://raw.githubusercontent.com/$dir_host/$download2", "$dir_local/$download2")
+Write-Host "`n`rDownloading: https://raw.githubusercontent.com/$dir_host/$download2`r`nDestination: $dir_local/$download2`n`r`n`r" -ForegroundColor Magenta 
+git clone "Downloading https://github.com/$repo_src_owner/$repo_src_name.git" --branch $repo_src_branch
 # navigate to directory of repo
-$null = New-Item -Path $repo_src_name -ItemType Directory -Force -ErrorAction SilentlyContinue 
 Set-Location $repo_src_name
 # return to original working dir
 git submodule update --force --recursive --init --remote
-$file = "../$download1"
+$file = "scripts/$download1"
+Write-Output $file
 powershell -Command $file
