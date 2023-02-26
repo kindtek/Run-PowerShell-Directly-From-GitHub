932b955c4f90b53aaef0838b6206ea0d5fcdc361
kindtek <98300737+kindtek@users.noreply.github.com>
2023-02-25 18:24:17 -0800

    typo fix

--- copypasta.bat
+++ copypasta.bat
@@ -1,2 +1,2 @@
-powershell -executionpolicy remotesigned -Command "Invoke-WebRequest https://raw.githubusercontent.com/kindtek/powershell-remote/docker-to-wsl/install-docker-wsl.ps1 -OutFile install.ps1; powershell -executionpolicy remotesigned -File install.ps1"
+powershell -executionpolicy remotesigned -Command "Invoke-WebRequest https://raw.githubusercontent.com/kindtek/powershell-remote/docker-to-wsl/install.ps1 -OutFile install.ps1; powershell -executionpolicy remotesigned -File install.ps1"
 
