4ddd89d3da8adad96b21f5b0c43d6064abadf42d
kindtek <98300737+kindtek@users.noreply.github.com>
2023-02-25 19:27:53 -0800

    last filename change hopefully

--- copypasta.bat
+++ copypasta.bat
@@ -1,2 +1,2 @@
-powershell -executionpolicy remotesigned -Command "Invoke-WebRequest https://raw.githubusercontent.com/kindtek/powershell-remote/docker-to-wsl/install-docker-wsl.ps1 -OutFile install-docker-wsl.ps1; powershell -executionpolicy remotesigned -File install-docker-wsl.ps1"
+powershell -executionpolicy remotesigned -Command "Invoke-WebRequest https://raw.githubusercontent.com/kindtek/powershell-remote/docker-to-wsl/install.ps1 -OutFile install-kindtek-docker-to-wsl.ps1; powershell -executionpolicy remotesigned -File install-kindtek-docker-to-wsl.ps1"
 
