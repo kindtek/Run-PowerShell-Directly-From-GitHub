6b85d9aff2b919423b3a2e07af186dd10d586ceb
kindtek <98300737+kindtek@users.noreply.github.com>
2023-02-25 19:21:10 -0800

    make clone script more dynamic 2.2

--- install.ps1
+++ install.ps1
@@ -5,8 +5,8 @@ $repo = "$repo_src_owner/$repo_src_name/$repo_src_branch"
 $download1 = "docker-wsl-install.ps1"
 $download2 = "get-latest-winget.ps1"
 $WebClient = New-Object System.Net.WebClient
-$WebClient.DownloadFile("https://raw.githubusercontent.com/$repo/scripts/$download1", "docker-wsl-install.ps1")
-$WebClient.DownloadFile("https://raw.githubusercontent.com/$repo/scripts/$download2", "get-latest-winget.ps1")
+$WebClient.DownloadFile("https://raw.githubusercontent.com/$repo/scripts/$download1", "$download1")
+$WebClient.DownloadFile("https://raw.githubusercontent.com/$repo/scripts/$download2", "$download2")
 git clone "https://github.com/$repo_src_owner/$repo_src_name.git" --branch $repo_src_branch
 # navigate to directory of repo
 $null = New-Item -Path $repo_src_name -ItemType Directory -Force -ErrorAction SilentlyContinue 
