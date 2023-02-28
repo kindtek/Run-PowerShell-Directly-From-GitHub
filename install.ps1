6b00ce392246e10650d2b391d005e9133230792f
kindtek <98300737+kindtek@users.noreply.github.com>
2023-02-28 00:05:10 -0800

    download extra file in case 'git clone' does not work

--- install.ps1
+++ install.ps1
@@ -10,8 +10,9 @@ $dir_host = "$repo_src_owner/$repo_src_name/$repo_src_branch/scripts"
 $dir_local = "$repo_src_name/scripts"
 $download1 = "docker-wsl-install.ps1"
 $download2 = "get-latest-winget.ps1"
+$download3 = "wsl-import.bat"
 $add_wsl_windows_features = "add-wsl-windows-features"
-$download3 = "$add_wsl_windows_features/$repo_src_name/add-features.ps1"
+$download4 = "$add_wsl_windows_features/$repo_src_name/add-features.ps1"
 
 # clear way for git clone
 if (Test-Path -Path "$PSScriptRoot/$repo_src_name-temp") {
@@ -31,9 +32,11 @@ Write-Host "`n`rDownloading: https://raw.githubusercontent.com/$dir_host/$downlo
 $WebClient.DownloadFile("https://raw.githubusercontent.com/$dir_host/$download1", "$dir_local/$download1")
 Write-Host "`n`rDownloading: https://raw.githubusercontent.com/$dir_host/$download2`r`nDestination: $dir_local/$download2" -ForegroundColor Magenta 
 $WebClient.DownloadFile("https://raw.githubusercontent.com/$dir_host/$download2", "$dir_local/$download2")
+Write-Host "`n`rDownloading: https://raw.githubusercontent.com/$dir_host/$download3`r`nDestination: $dir_local/$download3" -ForegroundColor Magenta 
+$WebClient.DownloadFile("https://raw.githubusercontent.com/$dir_host/$download3", "$dir_local/$download3")
 Push-Location $add_wsl_windows_features
-Write-Host "`n`rDownloading: https://raw.githubusercontent.com/$repo_src_owner/$download3`r`nDestination: $dir_local/$add_wsl_windows_features/add-features.ps1`n`r" -ForegroundColor Magenta 
-$WebClient.DownloadFile("https://raw.githubusercontent.com/$repo_src_owner/$download3", "$dir_local/$add_wsl_windows_features/add-features.ps1")
+Write-Host "`n`rDownloading: https://raw.githubusercontent.com/$repo_src_owner/$download4`r`nDestination: $dir_local/$add_wsl_windows_features/add-features.ps1`n`r" -ForegroundColor Magenta 
+$WebClient.DownloadFile("https://raw.githubusercontent.com/$repo_src_owner/$download4", "$dir_local/$add_wsl_windows_features/add-features.ps1")
 
 Pop-Location
 Pop-Location
