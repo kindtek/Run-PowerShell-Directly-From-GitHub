2a20bd4e1412c196729647d3e156a2f57b64614a
kindtek <98300737+kindtek@users.noreply.github.com>
2023-02-27 18:05:16 -0800

    cleanrer code with comments added

--- install.ps1
+++ install.ps1
@@ -1,5 +1,6 @@
+# powershell version compatibility for PSScriptRoot
 if (!$PSScriptRoot) { $PSScriptRoot = Split-Path $MyInvocation.MyCommand.Path -Parent }
-# start window at the bottom
+# jump to bottom line without clearing scrollback
 Write-Output "$([char]27)[2J"
 
 $repo_src_owner = 'kindtek'
@@ -19,18 +20,17 @@ if (Test-Path -Path "$PSScriptRoot/$repo_src_name-temp") {
 
 $WebClient = New-Object System.Net.WebClient
 
-# make directory tree for incoming repo
+# simulate structure of incoming repo
 $null = New-Item -Path "$repo_src_name" -ItemType Directory -Force -ErrorAction SilentlyContinue 
 Push-Location "$repo_src_name"
 $null = New-Item -Path scripts -ItemType Directory -Force -ErrorAction SilentlyContinue 
 Push-Location scripts
+$null = New-Item -Path $add_wsl_windows_features -ItemType Directory -Force -ErrorAction SilentlyContinue 
 
 Write-Host "`n`rDownloading: https://raw.githubusercontent.com/$dir_host/$download1`r`nDestination: $dir_local/$download1" -ForegroundColor Magenta 
 $WebClient.DownloadFile("https://raw.githubusercontent.com/$dir_host/$download1", "$dir_local/$download1")
 Write-Host "`n`rDownloading: https://raw.githubusercontent.com/$dir_host/$download2`r`nDestination: $dir_local/$download2" -ForegroundColor Magenta 
 $WebClient.DownloadFile("https://raw.githubusercontent.com/$dir_host/$download2", "$dir_local/$download2")
-
-$null = New-Item -Path $add_wsl_windows_features -ItemType Directory -Force -ErrorAction SilentlyContinue 
 Push-Location $add_wsl_windows_features
 Write-Host "`n`rDownloading: https://raw.githubusercontent.com/$repo_src_owner/$download3`r`nDestination: $dir_local/$add_wsl_windows_features/add-features.ps1`n`r" -ForegroundColor Magenta 
 $WebClient.DownloadFile("https://raw.githubusercontent.com/$repo_src_owner/$download3", "$dir_local/$add_wsl_windows_features/add-features.ps1")
