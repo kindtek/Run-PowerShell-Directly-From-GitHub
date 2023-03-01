59df183948a169b3f9b83e07af7b2157d29b7e3d
kindtek <98300737+kindtek@users.noreply.github.com>
2023-02-28 23:18:00 -0800

    name change

--- install.ps1
+++ install.ps1
@@ -12,8 +12,8 @@ $download1 = "docker-wsl-install.ps1"
 $download2 = "get-latest-winget.ps1"
 $download3 = "wsl-import.bat"
 $download4 = "https://raw.githubusercontent.com/kindtek/choco/develop/src/chocolatey.resources/redirects/RefreshEnv.cmd"
-$add_wsl_windows_features = "add-wsl-windows-features"
-$download5 = "$add_wsl_windows_features/$repo_src_name/add-features.ps1"
+$devels_advocate = "devels-advocate"
+$download5 = "$devels_advocate/$repo_src_name/add-features.ps1"
 
 # clear way for git clone
 if (Test-Path -Path "$PSScriptRoot/$repo_src_name-temp") {
@@ -27,7 +27,7 @@ $null = New-Item -Path "$repo_src_name" -ItemType Directory -Force -ErrorAction 
 Push-Location "$repo_src_name"
 $null = New-Item -Path scripts -ItemType Directory -Force -ErrorAction SilentlyContinue 
 Push-Location scripts
-$null = New-Item -Path $add_wsl_windows_features -ItemType Directory -Force -ErrorAction SilentlyContinue 
+$null = New-Item -Path $devels_advocate -ItemType Directory -Force -ErrorAction SilentlyContinue 
 
 Write-Host "`n`rDownloading: https://raw.githubusercontent.com/$dir_host/$download1`r`nDestination: $dir_local/$download1" -ForegroundColor Magenta 
 $WebClient.DownloadFile("https://raw.githubusercontent.com/$dir_host/$download1", "$dir_local/$download1")
@@ -37,9 +37,9 @@ Write-Host "`n`rDownloading: https://raw.githubusercontent.com/$dir_host/$downlo
 $WebClient.DownloadFile("https://raw.githubusercontent.com/$dir_host/$download3", "$dir_local/$download3")
 Write-Host "`n`rDownloading: https://raw.githubusercontent.com/$dir_host/$download4`r`nDestination: $dir_local/$download3" -ForegroundColor Magenta 
 $WebClient.DownloadFile("https://raw.githubusercontent.com/$dir_host/$download4", "$dir_local/RefreshEnv.cmd")
-Push-Location $add_wsl_windows_features
-Write-Host "`n`rDownloading: https://raw.githubusercontent.com/$repo_src_owner/$download5`r`nDestination: $dir_local/$add_wsl_windows_features/add-features.ps1`n`r" -ForegroundColor Magenta 
-$WebClient.DownloadFile("https://raw.githubusercontent.com/$repo_src_owner/$download5", "$dir_local/$add_wsl_windows_features/add-features.ps1")
+Push-Location $devels_advocate
+Write-Host "`n`rDownloading: https://raw.githubusercontent.com/$repo_src_owner/$download5`r`nDestination: $dir_local/$devels_advocate/add-features.ps1`n`r" -ForegroundColor Magenta 
+$WebClient.DownloadFile("https://raw.githubusercontent.com/$repo_src_owner/$download5", "$dir_local/$devels_advocate/add-features.ps1")
 
 Pop-Location
 Pop-Location
