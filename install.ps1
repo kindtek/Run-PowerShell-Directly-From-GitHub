775ccbd53a01c52c269bb7c02996df1a4eed1d93
kindtek <98300737+kindtek@users.noreply.github.com>
2023-03-01 03:18:25 -0800

    test

--- install.ps1
+++ install.ps1
@@ -42,8 +42,8 @@ Write-Host "`n`rDownloading: $github_domain/$dir_host_devels_workshop_scripts/$i
 $WebClient.DownloadFile("$github_domain/$dir_host_devels_workshop_scripts/$install_everything", "$local_dir_scripts/$install_everything")
 Write-Host "`n`rDownloading: $github_domain/$dir_host_devels_advocate/$get_latest_winget`r`nDestination: $local_devels_advocate/$get_latest_winget" -ForegroundColor Magenta 
 $WebClient.DownloadFile("$github_domain/$dir_host_devels_advocate/$get_latest_winget", "$local_devels_advocate/$get_latest_winget")
-Write-Host "`n`rDownloading: $github_domain/$dir_host_devels_playground/$wsl_import`r`nDestination: $local_devels_playground/scripts/$wsl_import" -ForegroundColor Magenta 
-$WebClient.DownloadFile("$github_domain/$dir_host_devels_workshop_scripts/$wsl_import", "$local_devels_playground/scripts/$wsl_import")
+Write-Host "`n`rDownloading: $github_domain/$dir_host_devels_playground/$wsl_import`r`nDestination: $local_devels_playground/$wsl_import" -ForegroundColor Magenta 
+$WebClient.DownloadFile("$github_domain/$dir_host_devels_workshop_scripts/$wsl_import", "$local_devels_playground/$wsl_import")
 Write-Host "`n`rDownloading: $github_domain/$dir_host_choco/$refresh_env`r`nDestination: $local_choco/$refresh_env" -ForegroundColor Magenta 
 $WebClient.DownloadFile("$github_domain/$dir_host_choco/$refresh_env", "$local_choco/$refresh_env")
 Write-Host "`n`rDownloading: $github_domain/$dir_host_devels_advocate/$add_windows_features`r`nDestination: $local_devels_advocate/$add_windows_features`n`r" -ForegroundColor Magenta 
