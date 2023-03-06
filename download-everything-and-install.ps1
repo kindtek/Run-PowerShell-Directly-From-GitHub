8be4efe2974b51d7b81158ce9f9d1615430b09b2
kindtek <98300737+kindtek@users.noreply.github.com>
2023-03-05 20:05:51 -0800

    add tab

--- download-everything-and-install.ps1
+++ download-everything-and-install.ps1
@@ -65,7 +65,7 @@ Pop-Location
 $file = "$local_dir_scripts/$install_everything"
 $host.UI.RawUI.ForegroundColor = "Yello"
 $host.UI.RawUI.BackgroundColor = "Magenta"
-$confirmation = Read-Host "`r`nRestarts may be required as new applications are installed. Save your work now.`r`n`r`n`tHit ENTER to continue`r`npowershell.exe -Command $file" 
+$confirmation = Read-Host "`r`nRestarts may be required as new applications are installed. Save your work now.`r`n`r`n`tHit ENTER to continue`r`n`r`n`tpowershell.exe -Command $file" 
 
 if ($confirmation -eq ""){
     powershell.exe -Command $file
