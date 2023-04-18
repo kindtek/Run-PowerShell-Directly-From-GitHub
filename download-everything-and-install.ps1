5c15a0c1b36a5fa57ac821eaae4cc7e18eb3cbfa
kindtek <98300737+kindtek@users.noreply.github.com>
2023-04-18 16:44:18 -0700

    refresh envs

--- download-everything-and-install.ps1
+++ download-everything-and-install.ps1
@@ -37,6 +37,8 @@ function install_repo {
         winget upgrade --id Git.Git --source winget --silent --locale en-US --accept-package-agreements --accept-source-agreements --disable-interactivity
         Write-Host "$software_name installed" | Out-File -FilePath "$git_parent_path/.github-installed"
         $new_install = $true
+        Invoke-WebRequest "https://raw.githubusercontent.com/kindtek/choco/ac806ee5ce03dea28f01c81f88c30c17726cb3e9/src/chocolatey.resources/redirects/RefreshEnv.cmd" -OutFile $file;
+        powershell.exe -executionpolicy remotesigned -File $file
     }
     else {
         Write-Host "$software_name already installed" 
