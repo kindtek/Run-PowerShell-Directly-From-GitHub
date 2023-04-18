186735d2db0ab1823737d6507b256e8b4c7449b1
kindtek <98300737+kindtek@users.noreply.github.com>
2023-04-18 04:00:57 -0700

    black and red

--- download-everything-and-install.ps1
+++ download-everything-and-install.ps1
@@ -107,8 +107,8 @@ do {
     $confirmation = ''
     if ($start_over -ine 's') {
         Write-Host "$([char]27)[2J"
-        $host.UI.RawUI.ForegroundColor = "Yellow"
-        $host.UI.RawUI.BackgroundColor = "Red"
+        $host.UI.RawUI.ForegroundColor = "Black"
+        $host.UI.RawUI.BackgroundColor = "DarkRed"
 
         # $confirmation = Read-Host "`r`nRestarts may be required as new applications are installed. Save your work now.`r`n`r`n`tHit ENTER to continue`r`n`r`n`tpowershell.exe -Command $file $args" 
         $confirmation = Read-Host "`r`nRestarts may be required as new applications are installed. Save your work now.`r`n`r`n`r`n`r`n`r`n`r`n`tHit ENTER to continue"
