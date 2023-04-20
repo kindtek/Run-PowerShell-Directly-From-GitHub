0f734e91d073dee819c022b174998f4419e209d4
kindtek <98300737+kindtek@users.noreply.github.com>
2023-04-20 05:50:53 -0700

    force quotation marks to be read by wsl-docker-import

--- download-everything-and-install.ps1
+++ download-everything-and-install.ps1
@@ -86,7 +86,7 @@ function run_devels_playground {
             # Write-Host "&$devs_playground $global:img_tag"
             # Write-Host "$([char]27)[2J"
             Write-Host "powershell.exe -Command \"$git_path/dvlp/scripts/wsl-docker-import.cmd\" \"$img_tag\" \"$non_interactive\" \"$default_distro\""
-            powershell.exe -Command "$git_path/dvlp/scripts/wsl-docker-import.cmd" "$img_tag" "$non_interactive" "$default_distro"
+            powershell.exe -Command "$git_path/dvlp/scripts/wsl-docker-import.cmd" \"$img_tag\" \"$non_interactive\" \"$default_distro\"
             # &$devs_playground = "$git_path/dvlp/scripts/wsl-docker-import.cmd $global:img_tag"
             # Write-Host "$software_name installed`r`n" | Out-File -FilePath "$git_path/.dvlp-installed"
         }
