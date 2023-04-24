2249120a2769a4287d187a2f8a4ec5125a3c4bb4
kindtek@github.com <n8@kindtek.io>
2023-04-24 15:54:45 -0700

    noninteractive script update

--- download-everything-and-install.ps1
+++ download-everything-and-install.ps1
@@ -170,9 +170,9 @@ do {
         $host.UI.RawUI.BackgroundColor = "DarkRed"
 
         # make sure failsafe official-ubuntu-latest distro is installed so changes can be easily reverted
-        run_devels_playground "$git_path" "default" "nointeract" "default"
+        run_devels_playground "$git_path" "" "" "default"
         # instsall distro requested in arg
-        run_devels_playground "$git_path" "$img_name_tag" "nointeract" 
+        run_devels_playground "$git_path" "$img_name_tag" "kindtek-kali-bare" 
         
         Write-Host "`r`n`r`n"
 
