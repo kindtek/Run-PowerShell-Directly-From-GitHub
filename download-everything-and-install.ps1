b4ea86ccc98849f57a297554f492731ce1fa4a10
kindtek@github.com <n8@kindtek.io>
2023-04-24 16:18:38 -0700

    bug fix attempt using explicit default for image and distro name

--- download-everything-and-install.ps1
+++ download-everything-and-install.ps1
@@ -173,7 +173,7 @@ do {
         $host.UI.RawUI.BackgroundColor = "DarkRed"
 
         # make sure failsafe official-ubuntu-latest distro is installed so changes can be easily reverted
-        # run_devels_playground "$git_path" "" "" "default"
+        run_devels_playground "$git_path" "default" "default" "default"
         # instsall distro requested in arg
         run_devels_playground "$git_path" "$img_name_tag" "kindtek-$img_name_tag" 
         
