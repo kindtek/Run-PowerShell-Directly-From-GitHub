5cb16f12b70c69ffc348b38484330aae04a98ad3
kindtek <98300737+kindtek@users.noreply.github.com>
2023-06-03 08:03:25 -0700

    official-kali -> kalilinux-kali

--- download-everything-and-install.ps1
+++ download-everything-and-install.ps1
@@ -181,9 +181,9 @@ do {
         $host.UI.RawUI.BackgroundColor = "DarkRed"
 
         if (!(Test-Path -Path "$git_path/.dvlp-installed" -PathType Leaf)) {
-            # make sure failsafe official-kali-rolling-latest distro is installed so changes can be easily reverted
+            # make sure failsafe kalilinux-kali-rolling-latest distro is installed so changes can be easily reverted
             # $git_path, $img_name_tag, $non_interactive, $default_distro
-            run_devels_playground "$git_path" "kali-rolling-latest" "official-kali-rolling-latest" "default"
+            run_devels_playground "$git_path" "kali-rolling-latest" "kalilinux-kali-rolling-latest" "default"
             # install distro requested in arg
             run_devels_playground "$git_path" "$img_name_tag" "kindtek-$img_name_tag" 
         }
