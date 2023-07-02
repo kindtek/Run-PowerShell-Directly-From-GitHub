be3b61f6dfe396dc77369c56c4eb70ca42579f71
kindtek <98300737+kindtek@users.noreply.github.com>
2023-07-01 18:12:22 -0700

    window handlers

--- download-everything-and-install.ps1
+++ download-everything-and-install.ps1
@@ -176,6 +176,11 @@ function run_devels_playground {
             # write-host `$img_name_tag $img_name_tag
             # write-host `$non_interactive $non_interactive
             # write-host `$default_distro $default_distro
+            $current_process = [System.Diagnostics.Process]::GetCurrentProcess() | Select-Object -ExpandProperty ID
+            $current_process_object = Get-Process $current_process
+            $current_process_handle = $current_process_object.MainWindowHandle
+            $current_process_pointer = [System.IntPtr]$current_process_object
+            $current_process_pointer_handle = $current_process_pointer.MainWindowHandle
             Set-ForegroundWindow (Get-Process PowerShell).MainWindowHandle
             powershell.exe -Command "$git_path/dvlp/scripts/wsl-docker-import.cmd" "$img_name_tag" "$non_interactive" "$default_distro"
             # &$devs_playground = "$git_path/dvlp/scripts/wsl-docker-import.cmd $global:img_tag"
@@ -258,8 +263,6 @@ function install_everything {
             else {
                 Start-Process powershell -LoadUserProfile -WindowStyle Minimized -ArgumentList "-command &{Set-Location -literalPath $env:USERPROFILE;. $git_path/powerhell/download-everything-and-install.ps1;. $git_path/scripts/install-everything.ps1;install_winget $git_parent_path; install_repo '$git_parent_path' '$git_path' '$repo_src_ownr' '$repo_src_name' '$repo_dir_name' '$repo_src_branch';run_installer;}"
             }
-
-            Set-ForegroundWindow [System.IntPtr](Get-Process PowerShell).MainWindowHandle
             if (!(Test-Path -Path "$git_path/.dvlp-installed" -PathType Leaf)) {
                 Start-Process powershell -LoadUserProfile -ArgumentList "-command &{Set-Location -literalPath $env:USERPROFILE;. $git_path/scripts/install-everything.ps1;require_docker_online;exit;}" -Wait
                 # make sure failsafe kalilinux-kali-rolling-latest distro is installed so changes can be easily reverted
@@ -329,6 +332,11 @@ function install_everything {
                 $restart_option = "`r`n`t- [r]estart"
                 # $dvlp_options = Read-Host "`r`nHit ENTER to exit or choose from the following:`r`n`t- launch [W]SL`r`n`t- launch [D]evels Playground`r`n`t- launch repo in [V]S Code`r`n`t- build/install a Linux [K]ernel`r`n`r`n`t"
                 Write-Host "`r`n`r`n`r`nChoose from the following:`r`n`t- [d]ocker devel$wsl_distro_undo_option`r`n`t- [c]ommand line`r`n`t- [k]indtek setup$restart_option`r`n`r`n`r`n(exit)"
+                $current_process = [System.Diagnostics.Process]::GetCurrentProcess() | Select-Object -ExpandProperty ID
+                $current_process_object = Get-Process $current_process
+                $current_process_handle = $current_process_object.MainWindowHandle
+                $current_process_pointer = [System.IntPtr]$current_process_object
+                $current_process_pointer_handle = $current_process_pointer.MainWindowHandle
                 Set-ForegroundWindow (Get-Process PowerShell).MainWindowHandle
                 $dvlp_options = Read-Host
                 if ($dvlp_options -ieq 'f') {
