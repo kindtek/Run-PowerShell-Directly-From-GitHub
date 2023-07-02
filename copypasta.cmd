c370badff2e445f6cd5f2562c6ba6fb65a2c9f52
kindtek <98300737+kindtek@users.noreply.github.com>
2023-07-02 15:11:57 -0700

    install-kindtek-devels-workshop -> dvlp

--- copypasta.cmd
+++ copypasta.cmd
@@ -1,2 +1,2 @@
-powershell.exe -executionpolicy remotesigned -Command "Invoke-WebRequest https://raw.githubusercontent.com/kindtek/powerhell/dvl-works/devel-spawn.ps1 -OutFile install-kindtek-devels-workshop.ps1; powershell.exe -executionpolicy remotesigned -File install-kindtek-devels-workshop.ps1 kali-gui-kernel"
+powershell.exe -Command "Invoke-WebRequest https://raw.githubusercontent.com/kindtek/powerhell/dvl-works/devel-spawn.ps1 -OutFile dvlp.ps1; powershell.exe -ExecutionPolicy RemoteSigned -File dvlp.ps1 kali-gui-kernel"
 
