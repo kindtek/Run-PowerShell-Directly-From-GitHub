617d47bd34f69b534be93558c3ec2e0ca7b13363
kindtek <98300737+kindtek@users.noreply.github.com>
2023-07-07 00:11:32 -0700

    command path

--- devel-spawn.ps1
+++ devel-spawn.ps1
@@ -13,7 +13,7 @@ try {
         $set_local_path_command = [string][System.Environment]::SetEnvironmentVariable('path', '$local_paths')
         $set_machine_path_command = [string][System.Environment]::SetEnvironmentVariable('path', '$machine_paths', [System.EnvironmentVariableTarget]::Machine)
         Start-Process -FilePath powershell.exe -ArgumentList "-Command $set_local_path_command"
-        Start-Process -FilePath powershell.exe -ArgumentList "-Command $set_machine_path_command)"
+        Start-Process -FilePath powershell.exe -ArgumentList "-Command $set_machine_path_command"
     
 }
 
