e1553e51b2907260bc269aed7b52c20e44d13559
kindtek <98300737+kindtek@users.noreply.github.com>
2023-07-15 10:23:13 -0700

    end test

--- devel-spawn.ps1
+++ devel-spawn.ps1
@@ -743,10 +743,10 @@ function test_wsl_distro {
     $test_string = 'helloworld'
     $test = wsl.exe -d $distro_name --exec echo $test_string
     if ($test -eq $test_string){
-        Write-Host "$distro_name is valid distro"
+        # Write-Host "$distro_name is valid distro"
         return $true
     } else {
-        Write-Host "$distro_name is INVALID distro"
+        # Write-Host "$distro_name is INVALID distro"
         return $false
     }
 }
@@ -761,11 +761,10 @@ function test_default_wsl_distro {
         Write-Host "testing wsl default distro $distro_name"
         require_docker_online_new_win
         if ((get_default_wsl_distro -eq $distro_name) -and (is_docker_desktop_online -eq $true)){
-            Write-Host "$distro_name is valid default distro"
-
+            # Write-Host "$distro_name is valid default distro"
             return $true
         } else {
-            Write-Host "$distro_name is INVALID default distro"
+            # Write-Host "$distro_name is INVALID default distro"
         }
     }
 
