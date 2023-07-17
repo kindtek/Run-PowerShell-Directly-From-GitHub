3df896a216f5e50d32198ce8ef1cfea23b280bca
kindtek <98300737+kindtek@users.noreply.github.com>
2023-07-17 06:41:23 -0700

    kw

--- devel-spawn.ps1
+++ devel-spawn.ps1
@@ -1053,8 +1053,8 @@ function install_everything {
                     Exit
                 }
             }
-            # img_tag must not empty ... OR dvlp must not installed
-            if (!(Test-Path -Path "$env:KINDTEK_WIN_DVLW_PATH/.dvlp-installed" -PathType Leaf) -or (!([string]::IsNullOrEmpty($img_tag)))) {
+            # if confirmation is kw and (img_tag must not empty ... OR dvlp must not installed)
+            if (($confirmation -ieq 'kw') -and (!(Test-Path -Path "$env:KINDTEK_WIN_DVLW_PATH/.dvlp-installed" -PathType Leaf) -or (!([string]::IsNullOrEmpty($img_tag))))) {
                 Write-Host "`t-- use CTRL + C or close this window to cancel anytime --"
                 Start-Sleep 3
                 Write-Host ""
