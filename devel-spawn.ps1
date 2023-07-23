38ab1d3e5c39b48e1a2fcd9b2aef5aac09582ba6
kindtek <98300737+kindtek@users.noreply.github.com>
2023-07-23 00:17:14 -0700

    img_name_tag

--- devel-spawn.ps1
+++ devel-spawn.ps1
@@ -1072,8 +1072,8 @@ function install_everything {
             do {
                 $wsl_restart_path = "$env:USERPROFILE/wsl-restart.ps1"
                 $env:DEFAULT_WSL_DISTRO = get_default_wsl_distro
-                if ((Test-Path -Path "$env:KINDTEK_WIN_DVLW_PATH/.dvlp-installed" -PathType Leaf) -and (!([string]::IsNullOrEmpty($image_name_tag)))){
-                    $run_devels_playground_noninteractive = "`r`n`t- [d!] install $image_name_tag)"
+                if ((Test-Path -Path "$env:KINDTEK_WIN_DVLW_PATH/.dvlp-installed" -PathType Leaf) -and (!([string]::IsNullOrEmpty($img_name_tag)))){
+                    $run_devels_playground_noninteractive = "`r`n`t- [d!] install $img_name_tag)"
                 }
                 if ([string]::IsNullOrEmpty($env:OLD_DEFAULT_WSL_DISTRO)) {
                     $env:OLD_DEFAULT_WSL_DISTRO = $env:KINDTEK_FAILSAFE_WSL_DISTRO
