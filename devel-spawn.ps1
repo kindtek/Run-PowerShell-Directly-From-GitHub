fabacd12a6b3102ad6c6df2cf1cf355f6b12184f
kindtek <98300737+kindtek@users.noreply.github.com>
2023-08-04 01:41:53 -0700

    force repo sync

--- devel-spawn.ps1
+++ devel-spawn.ps1
@@ -1113,7 +1113,7 @@ function install_everything {
                     # write-host 'dot sourcing devel tools'
                     . $env:KINDTEK_DEVEL_TOOLS
                 }
-                start_dvlp_process_pop "install_git;run_installer;" "" ""
+                start_dvlp_process_pop "install_git;run_installer;sync_repo;" "" ""
             } 
             do {
                 $wsl_restart_path = "$env:USERPROFILE/wsl-restart.ps1"
