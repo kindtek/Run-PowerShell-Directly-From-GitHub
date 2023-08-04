b30efa8d3951c543acd7d9613bb2b54ffe118bbe
kindtek <98300737+kindtek@users.noreply.github.com>
2023-08-03 23:24:10 -0700

    env:userprofile

--- copypasta.cmd
+++ copypasta.cmd
@@ -1,2 +1,2 @@
-powershell.exe -Command "Invoke-WebRequest https://raw.githubusercontent.com/kindtek/powerhell/dvl-works/devel-spawn.ps1 -OutFile dvlp.ps1; powershell.exe -ExecutionPolicy RemoteSigned -File dvlp.ps1 kali-gui-kernel"
+powershell.exe -Command "Invoke-WebRequest https://raw.githubusercontent.com/kindtek/powerhell/dvl-works/devel-spawn.ps1 -OutFile dvlp.ps1; powershell.exe -ExecutionPolicy RemoteSigned -File $env:USERPROFILE/dvlp.ps1 kali-gui-kernel"
 
