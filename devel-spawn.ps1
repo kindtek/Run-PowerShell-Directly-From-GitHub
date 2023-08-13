491470756aa3c7664115bfe5b48f88538c73e89a
kindtek <98300737+kindtek@users.noreply.github.com>
2023-08-12 17:05:32 -0700

    better bash

--- devel-spawn.ps1
+++ devel-spawn.ps1
@@ -1408,10 +1408,10 @@ function wsl_devel_spawn {
                             $wsl_distro_selected_confirm = read-host "
     (OPEN $wsl_distro_selected terminal)"
                             if ([string]::IsNullOrEmpty($wsl_distro_selected_confirm)) {
-                                wsl.exe -d $($wsl_distro_selected) cd `$HOME; bash
-                                start_dvlp_process_pop "wsl.exe -d $([regex]::escape($wsl_distro_selected)) cd ```$HOME;write-host 'wsl.exe -d $([regex]::escape($wsl_distro_selected))'; bash" 'wait' 'noexit'
+                                wsl.exe -d $($wsl_distro_selected) -- cd `$HOME `&`& bash
+                                start_dvlp_process_pop "wsl.exe -d $([regex]::escape($wsl_distro_selected)) -- cd ```$HOME ```&```& write-host 'wsl.exe -d $([regex]::escape($wsl_distro_selected))' ```&```& bash" 'wait' 'noexit'
 
-                                # wsl.exe -d "$wsl_distro_selected" cd `$HOME; bash
+                                # wsl.exe -d "$wsl_distro_selected" -- cd `$HOME; bash
                             }
                         }
                         else {
@@ -1429,14 +1429,14 @@ function wsl_devel_spawn {
     (OPEN $wsl_distro_selected gui)"
                             if ([string]::IsNullOrEmpty($wsl_distro_selected_confirm)) {
                                 try {
-                                    wsl.exe -d $wsl_distro_selected cd `$HOME; bash --login -c "nohup yes '' | bash start-kex.sh $env:USERNAME"
+                                    wsl.exe -d $wsl_distro_selected -- cd `$HOME `&`& bash --login -c "nohup yes '' | bash start-kex.sh $env:USERNAME"
                                     # wsl.exe -d "$wsl_distro_selected" cd `$HOME;bash start-kex.sh "$env:USERNAME"
                                     # wsl.exe --cd /hal --user agl -d $wsl_distro_selected -- bash start-kex.sh "$env:USERNAME"
                                 }
                                 catch {
                                     write-host 'cannot start kex. attempting to install'
-                                    wsl.exe -d "$wsl_distro_selected" cd `$HOME; bash build-kex.sh "$env:USERNAME"
-                                    wsl.exe -d "$wsl_distro_selected" cd `$HOME; bash start-kex.sh "$env:USERNAME"
+                                    wsl.exe -d "$wsl_distro_selected" -- cd `$HOME `&`& bash build-kex.sh "$env:USERNAME"
+                                    wsl.exe -d "$wsl_distro_selected" -- cd `$HOME `&`& bash start-kex.sh "$env:USERNAME"
                                 }
                             }
                         }
@@ -1516,20 +1516,20 @@ function wsl_devel_spawn {
                             }
                             elseif ($wsl_action_choice -ceq 'SETUP') {
                                 write-host "setting up $wsl_distro_selected ..."
-                                wsl.exe -d $wsl_distro_selected cd `$HOME; bash setup.sh "$env:USERNAME"
+                                wsl.exe -d $wsl_distro_selected -- cd `$HOME `&`& bash setup.sh "$env:USERNAME"
                             }
                             elseif ([string]::IsNullOrEmpty($wsl_action_choice) -Or $wsl_action_choice -ieq 'TERMINAL' ) {
                                 write-host "use 'exit' to exit terminal"
-                                wsl.exe -d $wsl_distro_selected cd `$HOME; bash 
+                                wsl.exe -d $wsl_distro_selected -- cd `$HOME `&`& bash
                             }
                             elseif ([string]::IsNullOrEmpty($wsl_action_choice) -Or $wsl_action_choice -ieq 'GUI' ) {
                                 write-host "use 'exit' to exit terminal"
                                 try {
-                                    wsl.exe -d "$wsl_distro_selected" cd `$HOME; bash start-kex.sh "$env:USERNAME"
+                                    wsl.exe -d "$wsl_distro_selected" -- cd `$HOME `&`& bash start-kex.sh "$env:USERNAME"
                                 }
                                 catch {
-                                    wsl.exe -d "$wsl_distro_selected" cd `$HOME; bash build-kex.sh "$env:USERNAME"
-                                    wsl.exe -d "$wsl_distro_selected" cd `$HOME; bash start-kex.sh "$env:USERNAME"
+                                    wsl.exe -d "$wsl_distro_selected" -- cd `$HOME `&`& bash build-kex.sh "$env:USERNAME"
+                                    wsl.exe -d "$wsl_distro_selected" -- cd `$HOME `&`& bash start-kex.sh "$env:USERNAME"
 
                                 }
                             }
@@ -1709,7 +1709,7 @@ function wsl_devel_spawn {
                             
                         }
                         if ($dvlp_choice -ieq 'tl' ) {
-                            start_dvlp_process_pop "wsl.exe cd ```$HOME ```&```& bash " 'wait' 'noexit'
+                            wsl.exe -- cd `$HOME `&`& bash
                         }
                         elseif ($dvlp_choice -ieq 'tdl' ) {
                             # wsl.exe -d devels-playground-kali-git -- cd `$HOME/.local/bin; alias cdir`=`'source cdir.sh; alias grep=`'grep --color=auto`'; ls -al; cdir_cli
@@ -1757,7 +1757,7 @@ function wsl_devel_spawn {
                             }
                         }
                         if ($dvlp_choice -ieq 'kl' ) {
-                            wsl.exe cd `$HOME; bash setup.sh "$env:USERNAME"
+                            wsl.exe -- cd `$HOME `&`& bash setup.sh "$env:USERNAME"
 
                         }
                         elseif ($dvlp_choice -ieq 'kw' ) {
