e292990d566bfa4cd64f5522bf1af96d8fac6787
kindtek <98300737+kindtek@users.noreply.github.com>
2023-08-14 12:06:14 -0700

    out-null

--- devel-spawn.ps1
+++ devel-spawn.ps1
@@ -1432,7 +1432,7 @@ function wsl_devel_spawn {
                     try {
                         $dvlp_input_orig = $dvlp_input
                         $dvlp_input = 'screen'
-                        Invoke-Expression $dvlp_input_orig | Out-String
+                        Invoke-Expression $dvlp_input_orig | Out-Null
                     } catch {
                         $dvlp_input = $dvlp_input_orig
                     }
