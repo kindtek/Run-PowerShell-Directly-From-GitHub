b711609880d84f77b1bc6d9a955a563fcfdfc47a
kindtek <98300737+kindtek@users.noreply.github.com>
2023-08-14 16:25:18 -0700

    skip prompt

--- devel-spawn.ps1
+++ devel-spawn.ps1
@@ -901,7 +901,7 @@ function get_repo_commit {
 }
 
 function reload_dvlp {
-    start-process -filepath powershell.exe -Verb RunAs -ArgumentList '-Command', "$($env:USERPROFILE)\dvlp.ps1 $($global:devel_spawn_args)" >> "$env:TEMP\spawnlogs.txt" 2>&1            
+    start-process -filepath powershell.exe -Verb RunAs -ArgumentList '-Command', "$($env:USERPROFILE)\dvlp.ps1 '$($global:devel_spawn_args)' 'skip prompt'" >> "$env:TEMP\spawnlogs.txt" 2>&1            
 }
 
 function update_dvlp {
@@ -1002,7 +1002,7 @@ function set_dvlp_auto_boot {
         set_dvlp_env 'KINDTEK_AUTO_BOOT' '1' 
         New-Item -Path "$env:AppData\Microsoft\Windows\Start Menu\Programs\Startup\dvlp-spawn.cmd" -Value "
         # PowerShell -Command `"Set-ExecutionPolicy Unrestricted`" >> `"$env:TEMP\spawnlogs.txt`" 2>&1
-        start wt -p windows cmd.exe /c echo 'please confirm administrator access to launch wsl devel' & powershell.exe start-process -filepath powershell.exe -Verb RunAs -WindowStyle Maximized -ArgumentList '-Command', '$($env:USERPROFILE)\dvlp.ps1 $($global:devel_spawn_args)' >> `"$env:TEMP\spawnlogs.txt`" 2>&1
+        start wt -p windows cmd.exe /c echo 'please confirm administrator access to launch wsl devel' & powershell.exe start-process -filepath powershell.exe -Verb RunAs -WindowStyle Maximized -ArgumentList '-Command', '$($env:USERPROFILE)\dvlp.ps1 `"$($global:devel_spawn_args)`" `"skip prompt`"' >> `"$env:TEMP\spawnlogs.txt`" 2>&1
         # PowerShell -Command `"Set-ExecutionPolicy RemoteSigned`" >> `"$env:TEMP\spawnlogs.txt`" 2>&1
         # cmd /k
         " -Force | Out-Null
@@ -1243,7 +1243,7 @@ function wsl_devel_spawn {
         $host.UI.RawUI.BackgroundColor = "Black"
 
         $confirmation = ''    
-        if (($dvlp_input -ine 'kw') -And (!(Test-Path -Path "$env:KINDTEK_WIN_GIT_PATH/.dvlp-installed" -PathType Leaf))) {          
+        if (($dvlp_input -ine 'kw') -And (!(Test-Path -Path "$env:KINDTEK_WIN_GIT_PATH/.dvlp-installed" -PathType Leaf)) -And [string]::IsNullOrEmpty($args[1])) {          
             try {
                 if (!($(dependencies_installed))) {
                     $host.UI.RawUI.ForegroundColor = "Black"
