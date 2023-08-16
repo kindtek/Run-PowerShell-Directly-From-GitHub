4e5f182523cded852f7a0720d6d66b3f990191d3
kindtek <98300737+kindtek@users.noreply.github.com>
2023-08-16 10:40:08 -0700

    better prompt - add location

--- devel-spawn.ps1
+++ devel-spawn.ps1
@@ -1536,11 +1536,13 @@ function wsl_devel_spawn {
                 if ($dvlp_prompt -eq $dvlp_prompt2){
                     # once activated, keep command line mode active 
                     $dvlp_prompt = $dvlp_prompt2
+                    $dvlp_location = "DVL $(get-location)"
                 } else {
                     $dvlp_prompt = $dvlp_prompt1
+                    $dvlp_location = ''
                 }
                 do {
-                    Write-Host -nonewline "$dvlp_options$dvlp_prompt"
+                    Write-Host -nonewline "$dvlp_options$dvlp_location$dvlp_prompt"
                     $dvlp_input = $Host.UI.ReadLine()
                     $dvlp_options = ''
                     if (($dvlp_input -ieq 'x') -Or ($dvlp_input -ieq 'exit') -Or (($dvlp_input -ieq '') -and ($dvlp_prompt -eq $dvlp_prompt1))) {
