05c41bd2519522353130deecd6477adb4a89c172
kindtek <98300737+kindtek@users.noreply.github.com>
2023-08-17 16:35:20 -0700

    better start process

--- devel-spawn.ps1
+++ devel-spawn.ps1
@@ -177,11 +177,11 @@ class dvlp_process {
     try {
       if ([string]::IsNullOrEmpty($this.proc_noexit)) {
         # write-host  "Start-Process -Filepath powershell.exe @proc_show -ArgumentList `"-Command`", `"$($this.proc_cmd)`""
-        Start-Process -Filepath powershell.exe -LoadUserProfile -WorkingDirectory $env:USERPROFILE @proc_show -ArgumentList '-Command', $this.proc_cmd
+        cmd.exe /c powershell Start-Process -Filepath powershell.exe -LoadUserProfile -WorkingDirectory $env:USERPROFILE @proc_show -ArgumentList '-Command', $this.proc_cmd
       }
       else {
         # Write-host "Start-Process -Filepath powershell.exe @proc_show -ArgumentList $($this.proc_noexit), '-Command', '$($this.proc_cmd)'"
-        Start-Process -Filepath powershell.exe -LoadUserProfile -WorkingDirectory $env:USERPROFILE @proc_show -ArgumentList $this.proc_noexit, '-Command', $this.proc_cmd
+        cmd.exe /c powershell Start-Process -Filepath powershell.exe -LoadUserProfile -WorkingDirectory $env:USERPROFILE @proc_show -ArgumentList $this.proc_noexit, '-Command', $this.proc_cmd
       }
     }
     catch { 
