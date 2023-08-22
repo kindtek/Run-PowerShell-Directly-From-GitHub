ba83b8868dbcde18a92675eac50b84cdf1c8603c
kindtek <98300737+kindtek@users.noreply.github.com>
2023-08-22 16:02:50 -0700

    debug better elevate

--- devel-spawn.ps1
+++ devel-spawn.ps1
@@ -1482,14 +1482,7 @@ function wsl_devel_spawn {
           " -ForegroundColor Yellow
           cmd.exe /c "timeout /t 3"
           try {
-            cmd.exe /c "ECHO powershell.exe start-process -filepath powershell.exe -ErrorAction SilentlyContinue -Verb RunAs -WindowStyle Maximized -ArgumentList '-Command', 'wt.exe /p /M cmd.exe powershell.exe -windowstyle maximized $($PSCommandPath) `"$($global:dvlp_arg0)`"  `"skip`"' & ^
-            timeout /t 3 ^
-            powershell.exe start-process -filepath powershell.exe -ErrorAction SilentlyContinue -Verb RunAs -WindowStyle Maximized -ArgumentList '-Command', 'wt.exe /p /M cmd.exe powershell.exe -windowstyle maximized $($PSCommandPath) `"$($global:dvlp_arg0)`"  `"skip`"' > NUL & ^
-            IF errorlevel 1 ( ^
-              exit /b ^
-            ) ELSE ( ^
-              exit 0 ^
-            )"
+            cmd.exe /c "ECHO powershell.exe start-process -filepath powershell.exe -ErrorAction SilentlyContinue -Verb RunAs -WindowStyle Maximized -ArgumentList '-Command', 'wt.exe /p /M cmd.exe powershell.exe -windowstyle maximized $($PSCommandPath) `"$($global:dvlp_arg0)`"  `"skip`"' & "
             
             # powershell.exe start-process -filepath 'powershell.exe' -ErrorAction SilentlyContinue -Verb RunAs -WindowStyle Hidden -ArgumentList '-Command', 'wt.exe /p /M cmd.exe powershell.exe -windowstyle maximized %USERPROFILE%\dvlp.ps1 `"%KINDTEK_AUTO_BOOT%`"  `"skip`"' > NUL
             # start wt.exe /p cmd.exe powershell.exe "$env:USERPROFILE\dvlp.ps1" "$env:KINDTEK_AUTO_BOOT"  "skip"
