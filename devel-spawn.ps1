4bc5362f588ff4a0514964e33d1bca0469be6845
kindtek <98300737+kindtek@users.noreply.github.com>
2023-08-22 16:53:49 -0700

    better elevate

--- devel-spawn.ps1
+++ devel-spawn.ps1
@@ -1482,7 +1482,7 @@ function wsl_devel_spawn {
           " -ForegroundColor Yellow
           cmd.exe /c "timeout /t 3"
           try {
-            cmd.exe /c "powershell.exe start-process -filepath powershell.exe -ErrorAction SilentlyContinue -Verb RunAs -WindowStyle Hidden -ArgumentList '-Command', 'wt.exe /p /M cmd.exe powershell.exe -windowstyle maximized -file $($PSCommandPath) `"$($global:dvlp_arg0)`"  `"skip`"'"
+            cmd.exe /c "powershell.exe start-process -filepath powershell.exe -ErrorAction SilentlyContinue -Verb RunAs -WindowStyle Hidden -ArgumentList '-Command', 'wt.exe /p /M cmd.exe powershell.exe -windowstyle maximized -file $($PSCommandPath) `"$($global:dvlp_arg0)`"  `"skip`" > NUL'"
             if ($LASTEXITCODE -ne 0) {
               throw
             }
