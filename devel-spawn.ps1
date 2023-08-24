b92e708daca0d8e61d4f1220e32885f95abc838b
kindtek <98300737+kindtek@users.noreply.github.com>
2023-08-24 15:55:49 -0700

    better prompt

--- devel-spawn.ps1
+++ devel-spawn.ps1
@@ -1745,7 +1745,7 @@ continue or skip
         try {
           $wsl_distro_list = get_wsl_distro_list
           display_wsl_distro_list $wsl_distro_list
-          $dvlp_options = "`r`n`r`n`r`nEnter a wsl distro number/name, powershell command, docker image (repo/image:tag), or one of the following:`r`n`r`n`t- [i]mport docker image into wsl${docker_devel_spawn_noninteractive}`r`n`t- [t]erminal`r`n`t- [m]aintenance`r`n`t- [update]$update_found`r`n`t- [screen]`r`n`t- [restart] wsl/docker`r`n`t${wsl_distro_revert_options}- [reboot] computer`r`n`t- [auto] boot is $auto_boot_status`r`n`r`n`r`n"
+          $dvlp_options = "`r`n`r`n`t- [n = distro number] open distro options`r`n`t-[repo/image:tag] import docker image into wsl`r`n`t- [i]mport docker image into wsl${docker_devel_spawn_noninteractive}`r`n`t- [powershell command]`r`n`t- [t]erminal`r`n`t- [m]aintenance`r`n`t- [update]$update_found`r`n`t- [screen]`r`n`t- [restart] wsl/docker`r`n`t${wsl_distro_revert_options}- [reboot] computer`r`n`t- [auto] boot is $auto_boot_status`r`n`r`n`r`n"
           if ($dvlp_input -eq 'screen' -and [string]::IsNullOrEmpty(($global:dvlp_arg1))) {
             #       write-host -nonewline ":)
 #   _ _ _ _ _ // 
@@ -1771,7 +1771,7 @@ continue or skip
               write-host "`r`n`r`n --------------------------------------------------------------------------`r`n`r`n"
             }
             display_wsl_distro_list $wsl_distro_list
-            $dvlp_options = "`r`n`r`n`r`nEnter a wsl distro number/name, powershell command, docker image (repo/image:tag), or one of the following:`r`n`r`n`t- [i]mport docker image into wsl${docker_devel_spawn_noninteractive}`r`n`t- [t]erminal`r`n`t- [m]aintenance`r`n`t- [update]$update_found`r`n`t- [screen]`r`n`t- [restart] wsl/docker`r`n`t${wsl_distro_revert_options}- [reboot] computer`r`n`t- [auto] boot is $auto_boot_status`r`n`r`n`r`n"
+            $dvlp_options = "`r`n`r`n`t- [n = distro number] open distro options`r`n`t-[repo/image:tag] import docker image into wsl`r`n`t- [i]mport docker image into wsl${docker_devel_spawn_noninteractive}`r`n`t- [powershell command]`r`n`t- [t]erminal`r`n`t- [m]aintenance`r`n`t- [update]$update_found`r`n`t- [screen]`r`n`t- [restart] wsl/docker`r`n`t${wsl_distro_revert_options}- [reboot] computer`r`n`t- [auto] boot is $auto_boot_status`r`n`r`n`r`n"
           }
           catch {
             if ($dvlp_input -eq 'screen' -and [string]::IsNullOrEmpty(($global:dvlp_arg1))) {
